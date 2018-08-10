ae40c3d2208c35e710b8c988847dc73e6eb5871a
Sharad Shingade <31508951+SharadShingade@users.noreply.github.com>
2018-08-10 09:26:52 +0530

    test
    cheanges

--- Bounding_box.py
+++ Bounding_box.py
@@ -14,6 +14,7 @@ if os.path.exists("bounding-boxes"):
     shutil.rmtree("bounding-boxes")
 os.mkdir("bounding-boxes")
 
+x=01
 spatialReference = osr.SpatialReference()
 spatialReference.SetWellKnownGeogCS('WGS84')
 
